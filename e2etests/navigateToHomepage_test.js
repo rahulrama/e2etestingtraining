1433cf090465b85f387f84c67181d2082533f7e2
Rahul Ramakrishna <rahulrama@users.noreply.github.com>
2019-09-12 14:13:46 +0100

    Updated the url for testing againts

--- e2etests/navigateToHomepage_test.js
+++ e2etests/navigateToHomepage_test.js
@@ -1,13 +1,13 @@
 Feature('navigateToAEVIDev');
 
 Scenario('navigate to the AEVI developer homepage', (I) => {
-    I.amOnPage('https://developer.aevi-test.com/');
+    I.amOnPage('https://developer.aevi.com/');
     I.see('Let’s create a better merchant experience'); 
     // broken test on purpose - fix it!
 });
 
 Scenario('navigate to the AEVI developer homepage', (I) => {
-    I.amOnPage('https://developer.aevi-test.com/');
+    I.amOnPage('https://developer.aevi.com/');
     I.click('Sign up');
     I.see('Terms of Use for AEVI Websites'); 
 });
