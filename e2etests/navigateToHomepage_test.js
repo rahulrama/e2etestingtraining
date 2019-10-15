034107e7e6cf446df78f1504199cfbc2162154c6
Rahul Ramakrishna <rahulrama@users.noreply.github.com>
2019-10-15 18:48:09 +0100

    updating scenario name for demo test

--- e2etests/navigateToHomepage_test.js
+++ e2etests/navigateToHomepage_test.js
@@ -6,7 +6,7 @@ Scenario('navigate to the AEVI developer homepage', (I) => {
     // broken test on purpose - fix it!
 });
 
-Scenario('navigate to the AEVI developer homepage', (I) => {
+Scenario('navigate to the sign up page', (I) => {
     I.amOnPage('https://developer.aevi.com/');
     I.click('Sign up');
     I.see('Terms of Use for AEVI Websites'); 
